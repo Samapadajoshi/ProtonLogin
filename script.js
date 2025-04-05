1b2e9531146bed930a84caee1af4a587b77e1997
Samapadajoshi <sampadaj630@gmail.com>
2025-04-05 12:29:28 +0530

    Files added success

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const options = document.getElementById("options");
+
+function showOptions() {
+    options.style.right = "0vw";
+}
+
+function hideOptions() {
+    options.style.right = "-40vw";
+}
